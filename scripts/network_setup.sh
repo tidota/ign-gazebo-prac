10cd4937aa05dcc993597dcc5005aefb266e5535
Tetsuya Idota <tidota@hawaii.edu>
2020-12-19 18:52:52 -1000

    add script to setup network

---
+++ scripts/network_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script setups a local machine running a gzclient accessing the master
+# which is running a gzserver and roscore
+export ROS_MASTER_URI=http://seilon-3:11311
+export ROS_IP=`hostname -I | awk '{print $1}'`
+export ROS_HOSTNAME=$ROS_IP
+#export GAZEBO_MASTER_URI=http://seilon-3:11346
+#export GAZEBO_IP=`hostname -I | awk '{print $1}'`
+export IGN_IP=`hostname -I | awk '{print $1}'`
