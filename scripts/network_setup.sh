525d8dfccf7be692e19eec4d53210b1d6e84fa80
Tetsuya Idota <tidota@hawaii.edu>
2021-02-14 02:10:43 -1000

    organize the network script

--- scripts/network_setup.sh
+++ scripts/network_setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # This script setups a local machine running a gzclient accessing the master
 # which is running a gzserver and roscore
-export ROS_MASTER_URI=http://seilon-3:11311
+SERVER_NAME=seilon-3
+
+# settings for ROS
+export ROS_MASTER_URI=http://$SERVER_NAME:11311
 export ROS_IP=`hostname -I | awk '{print $1}'`
 export ROS_HOSTNAME=$ROS_IP
-#export GAZEBO_MASTER_URI=http://seilon-3:11346
-#export GAZEBO_IP=`hostname -I | awk '{print $1}'`
-export IGN_RELAY=`nslookup seilon-3 | grep Address | tail -1 | awk '{print $2}'`
+
+# settings for ignition gazebo
+# reference: https://ignitionrobotics.org/api/transport/8.1/relay.html
+export IGN_RELAY=`nslookup $SERVER_NAME | grep Address | tail -1 | awk '{print $2}'`
 export IGN_IP=`hostname -I | awk '{print $1}'`
 export IGN_PARTITION=relay
